35d3620f736e50bf70aa1ac9772065ff3ad1bf4b
Dawid Zubrowski <dawid.zubrowski@vattenfall.com>
2026-06-24 17:07:04 +0200

    first commit

---
+++ src/main/kotlin/com/navyblue/sport_matcher_profiles/SportMatcherProfilesApplication.kt
@@ -0,0 +1,11 @@
+package com.navyblue.sport_matcher_profiles
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SportMatcherProfilesApplication
+
+fun main(args: Array<String>) {
+	runApplication<SportMatcherProfilesApplication>(*args)
+}
---
+++ src/test/kotlin/com/navyblue/sport_matcher_profiles/SportMatcherProfilesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.navyblue.sport_matcher_profiles
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SportMatcherProfilesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
